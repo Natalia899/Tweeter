3eec4c5c4cecc79691826346268fcf6b1bab3c0a
Natalia <nataliostapiuk@gmail.com>
2020-11-05 17:52:56 +0200

    controller

--- main.js
+++ main.js
@@ -2,3 +2,24 @@ const tweeter = Tweeter()
 const renderer = Renderer()
 
 renderer.renderPosts(tweeter.getPosts())
+$('#post').on('click', function () {
+    tweeter.addPost($('#input').val())
+    renderer.renderPosts(tweeter.getPosts())
+})
+
+$('#posts').on('click', '.delete', function () {
+    
+   tweeter.removePost($(this).closest('.post').data().id)
+
+    renderer.renderPosts(tweeter.getPosts())
+})
+$('#posts').on('click', '.addComment', function () {
+    console.log($(this).closest('#newcomment').find('.newComment'))
+    tweeter.addComment($(this).closest('.post').find('.newcomment').val(), $(this).closest('.post').data().id)
+    renderer.renderPosts(tweeter.getPosts())
+})
+$('#posts').on('click', '.delete-comment', function () {
+
+    tweeter.removeComment($(this).closest('.post').data().id, $(this).closest('.comment').data().id )
+    renderer.renderPosts(tweeter.getPosts())
+})
--- render.js
+++ render.js
@@ -2,16 +2,41 @@ const Renderer = function() {
    
   const renderPosts = function(posts) { 
     $('#posts').empty()
+    //console.log(posts)
     for (let post of posts) {
             
-            $('#posts').append(`<div class='post'> ${post.text}  </div>`)
+            // $('#posts').append(
+              const newPost = $(
+              `
+            <div class='post' data-id='${post.id}'>
+                <span>${post.text}</span>
+                <span class='delete'>Delete post</span>
+                <div id='newComment'> 
+                    <input class='newComment' type="text" placeholder="Comment"> 
+                    <span class='addComment'>Add comment</span> 
+                </div>
+            </div> `) 
+            
             for (let comment of post.comments)
              {
-               $('.post').append(`<div class='comments'> ${comment.text} </div>`)
+               const newComment= $ (
+               `<div class='comments '> <div class='comment'  data-id='${comment.id}' >
+                <span class='commentText'>  ${comment.text}</span>
+
+               <span class="delete-comment">X</span>   
+             </div> </div>`)
+             newPost.append(newComment)
             }
-        }
+            $('#posts').append(newPost)
+        } 
+
+
+        
+      }
     
-   } 
+   
    return { renderPosts
    }
 }
+
+
